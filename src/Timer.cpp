9e8b0cc9b189ef1ccc06eb936e40b9b05fd35ae1
Dominic <owerwa@gmail.com>
2025-06-12 23:31:53 +0200

    Timer added

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -1,1 +1,54 @@
 #include "Timer.hpp"
+
+Timer::Timer() : startTicks(0), pausedTicks(0), paused(false), started(false) {}
+
+void Timer::start() {
+    started = true;
+    paused = false;
+    startTicks = SDL_GetTicks();
+    pausedTicks = 0;
+}
+
+void Timer::stop() {
+    started = false;
+    paused = true;
+    startTicks = 0;
+    pausedTicks = 0;
+}
+
+void Timer::pause() {
+    if (started && !paused) {
+        paused = true;
+        pausedTicks = SDL_GetTicks() - startTicks;
+        startTicks = 0;
+    }
+}
+
+void Timer::unpause() {
+    if (started && paused) {
+        paused = false;
+        startTicks = SDL_GetTicks() - pausedTicks;
+        pausedTicks = 0;
+    }
+}
+
+Uint32 Timer::getTicks() const {
+    Uint32 time = 0;
+
+    if (started) {
+        if (paused) {
+            time = pausedTicks;
+        } else {
+            time = SDL_GetTicks() - startTicks;
+        }
+    }
+    return time;
+}
+
+bool Timer::isStarted() const {
+    return started;
+}
+
+bool Timer::isPaused() const {
+    return paused && started;
+}
