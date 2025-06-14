0b576d65223249f139369a33e613ef84935d2ca5
Dominic <owerwa@gmail.com>
2025-06-14 23:44:12 +0200

    changed Character class

--- include/ACharacter.hpp
+++ include/ACharacter.hpp
@@ -1,16 +1,34 @@
 #pragma once
 
 #include "AEntity.hpp"
+#include <vector>
+#include <unordered_map>
 
 class Tile;
 
+enum class AnimationState { Idle, Run, Attack };
+
+struct Animation {
+    std::vector<SDL_Rect> frames;
+    int frameDuration;
+};
+
 class ACharacter : public AEntity {
     protected:
+        // map of animations
+        std::unordered_map<AnimationState, Animation> animations;
+
+        AnimationState currentAnimation = AnimationState::Idle;
+        int currentFrameIndex = 0;
+        Uint32 lastFrameTime = 0;
+    
         // velocity
         int velX_;
         int velY_;
+
         // Maximum axis velocity
         const int maxVel_;
+
     public:
         ACharacter();
         virtual ~ACharacter();
@@ -18,4 +36,7 @@ class ACharacter : public AEntity {
         // moves and cheks for collision
         void move(Tile* tiles[]);
 
+        void update(Uint32 currentTime);
+        void render(SDL_Renderer* renderer, SDL_Rect& desRect);
+        void setAnimatino(AnimationState newState);
 };
